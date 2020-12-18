714274d87ff395559096204959902bf6c0384554
mikolas <mikolas.janota@gmail.com>
2020-12-18 19:34:56 +0100

    Starting to refactor  quantifier term enumeration.

---
+++ src/theory/quantifiers/term_tuple_enumerator.h
@@ -0,0 +1,24 @@
+/*
+ * File:  term_tuple_enumerator.h
+ * Author:  mikolas
+ * Created on:  Fri Dec 18 14:26:54 CET 2020
+ * Copyright (C) 2020, Mikolas Janota
+ */
+#ifndef TERM_TUPLE_ENUMERATOR_H_7640
+#define TERM_TUPLE_ENUMERATOR_H_7640
+#include "theory/quantifiers_engine.h"
+namespace CVC4 {
+namespace theory {
+namespace quantifiers {
+class TermTupleEnumeratorInterface {
+  public:
+    virtual bool next() = 0;
+    virtual ~TermTupleEnumeratorInterface() = default;
+};
+
+TermTupleEnumeratorInterface * mkTermTupleEnumerator(QuantifiersEngine *qe, Node quantifier, bool fullEffort, bool isRd);
+
+} /* CVC4::theory::quantifiers namespace */
+} /* CVC4::theory namespace */
+} /* CVC4 namespace */
+#endif /* TERM_TUPLE_ENUMERATOR_H_7640 */
